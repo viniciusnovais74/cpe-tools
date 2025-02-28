9aa8fe36acb32750e862e0d471fb38ec0112836f
PostMaster <postmaster@familianovais.com.br>
2025-02-28 12:01:55 -0300

    Testing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,7 +17,8 @@ const eslintConfig = [
       '@next/next/no-page-custom-font': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars':'off',
-      'react-hooks/exhaustive-deps':'off'
+      'react-hooks/exhaustive-deps':'off',
+      '@typescript-eslint/no-unused-expressions':'off'
     }
   })
 ];
